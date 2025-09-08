c929eed5bcbf172e796360658dcf2a550738882c
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2025-09-08 02:08:00 +0200

    fix: init

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ for chart in opsolving/*/; do
   helm dependency update "$chart" || true
   helm package "$chart" -d docs
 done
+
+helm repo index docs --url https://opsolving.github.io/charts
+
+git add .
